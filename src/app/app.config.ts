bd06e0f75b457ed5ea3f048238db0286c78f4efd
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2026-01-01 20:16:35 +0000

    Fixed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, HttpInterceptorFn } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
 import { authReducer, messagesReducer, configReducer, adminReducer } from './store/reducers/app.reducer';
 import { AppEffects } from './store/effects/app.effects';
-import { AuthInterceptor } from './core/interceptors/auth.interceptor';
-import { httpInterceptorFn } from '@angular/common/http';
 
 // Convert class-based interceptor to functional interceptor
-const authInterceptorFn: ReturnType<typeof httpInterceptorFn> = (req, next) => {
+const authInterceptorFn: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
   
   if (token) {
