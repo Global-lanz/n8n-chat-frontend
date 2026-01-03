af429a4fb46d99aad286897378e5a792ee89b80d
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2026-01-03 20:07:33 +0000

    Removed registering process

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
 import { ChatContainerComponent } from './features/chat/chat-container/chat-container.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { AdminGuard } from './core/guards/admin.guard';
@@ -8,7 +7,6 @@ import { AdminGuard } from './core/guards/admin.guard';
 export const routes: Routes = [
   { path: '', redirectTo: '/chat', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: 'chat', component: ChatContainerComponent, canActivate: [AuthGuard] },
   {
     path: 'settings',
